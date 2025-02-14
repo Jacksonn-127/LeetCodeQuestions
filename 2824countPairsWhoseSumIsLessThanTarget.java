947cec4422d15bd466ef5efcff46d25136f10099
Jackson <jacksonnuckles@gmail.com>
2025-02-14 16:00:19 -0500

    Create 2824countPairsWhoseSumIsLessThanTarget.java

---
+++ 2824countPairsWhoseSumIsLessThanTarget.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int countPairs(List<Integer> nums, int target) {
+        int returnValue = 0;
+
+        for(int i = 0; i < nums.size(); i++){
+            for(int j = i + 1; j < nums.size(); j++){
+                int temp = nums.get(i) + nums.get(j);
+                if(temp < target){
+                    returnValue++;
+                }
+            }
+        }
+
+        return returnValue;
+    }
+}
